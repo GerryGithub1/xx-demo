3f75f13fa07f8c72272c894be82c289ff3c30f2f
admin <gerry123>
2020-07-08 11:44:31 +0800

    Initial commit

---
+++ src/main/java/com/example/xxdemo/XxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xxdemo/XxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
